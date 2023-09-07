9ce5ba3529da61df304001c01baa63719e09bff4
kweidner14 <kyle.weidner14@protonmail.com>
2023-09-07 16:00:46 -0600

    feat: Enhance URL & IP Entry Logging and Add Export Functionality
    Initialize database tables on plugin activation if they don't exist
    Remove error logging for rate limit exceeded cases
    Store user IP in database when rate limit is reached
    Update result mapping logic
    Log all searched URLs and corresponding results in the database
    Implement function to export logged URLs and IPs to separate CSV files
    Add admin menu item 'IsItWP Export to CSV' under Tools
    Create admin page for exporting logged data to CSV

--- plugin.php
+++ plugin.php
@@ -16,6 +16,41 @@ function isitwp_enqueue_style() {
 }
 add_action( 'wp_enqueue_scripts', 'isitwp_enqueue_style' );
 
+// URL & IP Entry Logging
+// Check if custom database table exists, if not, create one.
+// Create new database table
+function isitwp_create_tables() {
+    global $wpdb;
+    $table_name = $wpdb->prefix . 'isitwp_urls';
+    $table_name_2 = $wpdb->prefix . 'isitwp_ips';
+
+    // Check if table already exists
+    if ($wpdb->get_var("SHOW TABLES LIKE '$table_name'") != $table_name) {
+        $sql = "CREATE TABLE $table_name (
+            id INT(11) AUTO_INCREMENT PRIMARY KEY,
+            url VARCHAR(255) NOT NULL,
+            result TEXT NOT NULL,
+            created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+        );";
+
+        require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
+        dbDelta( $sql );
+    }
+
+    if ($wpdb->get_var("SHOW TABLES LIKE '$table_name_2'") != $table_name_2) {
+        $sql2 = "CREATE TABLE $table_name_2 (
+            id INT(11) AUTO_INCREMENT PRIMARY KEY,
+            ip TEXT NOT NULL,
+            created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+        );";
+
+        require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
+        dbDelta( $sql2 );
+    }
+}
+// Run the function when the plugin is activated
+register_activation_hook( __FILE__, 'isitwp_create_tables' );
+
 function isitwp_check($atts = [], $content = null, $tag = '') {
 
     // Define the rate limit
@@ -39,8 +74,15 @@ function isitwp_check($atts = [], $content = null, $tag = '') {
     $rate_limit_count = get_transient($rate_limit_key);
 
     if ($rate_limit_count >= $rate_limit) {
-        // Logging the rate-limit violation could be useful to identify potentially malicious users
-        error_log("Rate limit reached for IP: " . $user_ip_concatenated);
+        // Insert URL into database table
+        global $wpdb;
+        $table_name = $wpdb->prefix . 'isitwp_ips';
+        $wpdb->insert(
+            $table_name,
+            array(
+                'ip' => $user_ip_concatenated
+            )
+        );
         return esc_html("You have reached your rate limit. Please wait before trying again.");
     }
 
@@ -68,11 +110,38 @@ function isitwp_check($atts = [], $content = null, $tag = '') {
                 set_transient($rate_limit_key, ($rate_limit_count + 1), $rate_time);
             }
 
+            // Map results to output
+            if ($result == 1) {
+                $result_output = "This website was built using WordPress.com";
+                $db_output = "WordPress.com";
+            } elseif ($result == 2) {
+                $result_output = "This website was built using WordPress.org";
+                $db_output = "WordPress.org";
+            } elseif($result == 3) {
+                $result_output = "This website doesn't appear to be built using WordPress. Some web hosts attempt to obscure that a website is built with WordPress. If you believe the website is built with WordPress, please try analyzing a different page of the website.";
+                $db_output = "Not WordPress";
+            } else {
+                $result_output = "Invalid URL. Please make sure the URL is correct and try again.";
+                $db_output = "Invalid URL";
+            }
+
             // Display results
             echo "<div class='analysis-results'>
             <p><span style='font-weight:bold;'>Website Analyzed:</span> " . esc_html($url) . "</p>
-            <p><span style='font-weight:bold;'>Result:</span> " . esc_html($result) . "</p>
+            <p><span style='font-weight:bold;'>Result:</span> " . esc_html($result_output) . "</p>
         </div>";
+
+            // Insert URL into database table
+            global $wpdb;
+            $table_name = $wpdb->prefix . 'isitwp_urls';
+            $wpdb->insert(
+                $table_name,
+                array(
+                    'url' => $url,
+                    'result' => $db_output
+                )
+            );
+
         } else {
             // The nonce is invalid, do not process the form
             echo 'Security check failed!';
@@ -103,11 +172,13 @@ function check_wordpress($url) {
     // Check if the website is a subdomain of wordpress.com
     if (strpos($parsed_url['host'], 'wordpress.com') !== false) {
         curl_close($ch);
-        return esc_html("This website was built using WordPress.com");
+//        return esc_html("This website was built using WordPress.com");
+        return 1;
     }
 
     if (filter_var($url, FILTER_VALIDATE_URL) === false) {
-        return esc_html("Invalid URL. Please make sure the URL is correct and try again.");
+//        return esc_html("Invalid URL. Please make sure the URL is correct and try again.");
+        return 4;
     }
 
     // Set the URL
@@ -132,13 +203,15 @@ function check_wordpress($url) {
     // If both attempts fail, return an error message
     if ($content === false) {
         curl_close($ch);
-        return esc_html("Invalid URL. Please make sure the URL is correct and try again.");
+//        return esc_html("Invalid URL. Please make sure the URL is correct and try again.");
+        return 4;
     }
 
     // Check for WordPress.com-specific script source
     if (strpos($content, 's0.wp.com') !== false) {
         curl_close($ch);
-        return esc_html("This website was built using WordPress.com");
+//        return esc_html("This website was built using WordPress.com");
+        return 1;
     }
 
     // Check for WordPress.org specific meta tags and WordPress-specific HTML attributes
@@ -146,13 +219,114 @@ function check_wordpress($url) {
         strpos($content, 'wp-content') !== false ||
         strpos($content, 'wp-includes') !== false) {
         curl_close($ch);
-        return esc_html("This website was built using WordPress.org");
+//        return esc_html("This website was built using WordPress.org");
+        return 2;
     }
 
     curl_close($ch);
 
-    return esc_html("This website doesn't appear to be built using WordPress. Some web hosts attempt to obscure that a website is built with WordPress. If you believe the website is built with WordPress, please try analyzing a different page of the website.");
+//    return esc_html("This website doesn't appear to be built using WordPress. Some web hosts attempt to obscure that a website is built with WordPress. If you believe the website is built with WordPress, please try analyzing a different page of the website.");
+    return 3;
 }
+
+// Function to export URLs and Results to a CSV file
+function isitwp_export_to_csv($suffix) {
+    global $wpdb;
+    $table_name = $wpdb->prefix . 'isitwp_' . $suffix;
+
+    $rows = $wpdb->get_results("SELECT * FROM $table_name");
+
+    if ($rows) {
+        if($suffix == 'urls') {
+            $filename = 'isitwp_urls_export.csv';
+        }
+
+        if($suffix == 'ips') {
+            $filename = 'isitwp_ips_export.csv';
+        }
+
+        header('Content-Type: text/csv');
+        header('Content-Disposition: attachment; filename=' . $filename);
+
+        $fp = fopen('php://output', 'wb');
+
+        // Add the header of the CSV
+        if($suffix == 'urls') {
+            $header = ['ID', 'URL', 'Result', 'Created At'];
+        }
+        if($suffix == 'ips') {
+            $header = ['ID', 'IP', 'Created At'];
+        }
+
+        fputcsv($fp, $header);
+
+        // Add the rows to the CSV
+        foreach ($rows as $row) {
+            if($suffix == 'urls') {
+                fputcsv($fp, [$row->id, $row->url, $row->result, $row->created_at]);
+            }
+
+            if($suffix == 'ips') {
+                fputcsv($fp, [$row->id, $row->ip, $row->created_at]);
+            }
+        }
+
+        fclose($fp);
+        exit;
+    } else {
+        echo 'No records found!';
+    }
+}
+
+// Add admin menu for export function
+function isitwp_admin_menu() {
+    add_submenu_page(
+        'tools.php',
+        'IsItWP Export to CSV',
+        'IsItWP Export',
+        'manage_options',
+        'isitwp_export',
+        'isitwp_export_admin_page'
+    );
+}
+add_action('admin_menu', 'isitwp_admin_menu');
+
+// Admin page content
+function isitwp_export_admin_page() {
+    echo '
+        <div class="wrap">
+            <h1>Export IsItWP Data to CSV</h1>
+            <div>
+                <h2>Export URLs</h2>
+                <form method="post" class="isitwp_export_form">
+                    <input type="hidden" name="isitwp_export_urls" value="1" />
+                    <input type="submit" value="Download CSV" />
+                </form>
+            </div>
+            <div>
+                <h2>Export IPs</h2>
+                <form method="post" class="isitwp_export_form">
+                    <input type="hidden" name="isitwp_export_ips" value="1" />
+                    <input type="submit" value="Download CSV" />
+                </form>
+            </div>
+        </div> ';
+}
+
+// Check if export button clicked and then export
+function isitwp_export_urls() {
+    if ( isset($_POST['isitwp_export_urls']) ) {
+        isitwp_export_to_csv('urls');
+    }
+}
+add_action('init', 'isitwp_export_urls');
+
+function isitwp_export_ips() {
+    if ( isset($_POST['isitwp_export_ips']) ) {
+        isitwp_export_to_csv('ips');
+    }
+}
+add_action('init', 'isitwp_export_ips');
 
 // Create shortcode for the form [isitwp_check]
 add_shortcode('isitwp_check', 'isitwp_check');
