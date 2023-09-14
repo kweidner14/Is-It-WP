ed53d8093a90e6103c585971b0eb9c65dc555b60
kweidner14 <kyle.weidner14@protonmail.com>
2023-09-14 11:54:49 -0600

    Adjust IP collection for rate limiting

--- includes/admin/class-rate-limit.php
+++ includes/admin/class-rate-limit.php
@@ -5,18 +5,12 @@ function rate_limiting()
     $rate_limit = 10; // 10 requests
     $rate_time = 60;  // within 60 seconds
 
-    // Concatenate various sources of IPs
-    $ip_keys = ['REMOTE_ADDR', 'HTTP_CLIENT_IP', 'HTTP_X_FORWARDED_FOR'];
-    $user_ip_concatenated = '';
-
-    foreach ($ip_keys as $key) {
-        if (isset($_SERVER[$key])) {
-            $user_ip_concatenated .= $_SERVER[$key];
-        }
-    }
+    // Get the user's IP
+    // Note: May need to adjust this in the future to deal with VPNs and bots/bad actors
+    $ip = $_SERVER['REMOTE_ADDR'];
 
     // Hash the concatenated IPs for a more secure and uniform key
-    $rate_limit_key = 'rate_limit_' . md5($user_ip_concatenated);
+    $rate_limit_key = 'rate_limit_' . md5($ip);
 
     // Rate Limiting
     $rate_limit_count = get_transient($rate_limit_key);
@@ -24,7 +18,7 @@ function rate_limiting()
     // Check if limit has been reached
     if ($rate_limit_count > $rate_limit) {
         // Insert IP into database table
-        insert_ip_to_db(esc_html('isitwp_ips'), $user_ip_concatenated);
+        insert_ip_to_db(esc_html('isitwp_ips'), $ip);
         return [
             'status' => 'limited',
             'message' => "You have reached your rate limit. Please wait before trying again.",
